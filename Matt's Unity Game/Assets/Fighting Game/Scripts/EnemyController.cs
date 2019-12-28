37f04e1970ff49038a11357ceb086991ad6130e2
Matthew Marshall <marsh189@mail.chapman.edu>
2019-12-28 09:47:05 -0800

    Adding enemies

---
+++ Matt's Unity Game/Assets/Fighting Game/Scripts/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float maxHealth;
+    public Slider healthBar;
+
+    float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(float dmg)
+    {
+        
+    }
+}
