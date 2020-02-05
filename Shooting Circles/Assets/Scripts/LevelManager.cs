562ec59e19209f13bf4b60f0c2da3d8c2b24de3c
Matthew Marshall <marsh189@mail.chapman.edu>
2020-02-05 02:50:32 -0800

    Pausing System

--- Shooting Circles/Assets/Scripts/LevelManager.cs
+++ Shooting Circles/Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
@@ -11,7 +12,10 @@ public class LevelManager : MonoBehaviour
     public float waveCount;
     public string gameState;
     public Texture2D cursor;
+    public GameObject pauseCanvas;
+    public Text text;
 
+    public float timeLeft = 5f;
     float nextSpawn;
 
     // Start is called before the first frame update
@@ -20,7 +24,7 @@ public class LevelManager : MonoBehaviour
 
         nextSpawn = Time.time;
         Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
-        gameState = "Playing";
+        gameState = "Starting";
     }
 
     // Update is called once per frame
@@ -30,10 +34,27 @@ public class LevelManager : MonoBehaviour
         {
             SpawnWave();
         }
-
-        if(Input.GetKeyDown(KeyCode.P))
+        if(gameState == "Paused")
         {
-            gameState = "Paused";
+            pauseCanvas.SetActive(true);
+        }
+        if(gameState == "Starting")
+        {
+            CountDown();
+        }
+
+        if(Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameState == "Playing")
+            {
+                gameState = "Paused";
+            }
+            else if(gameState == "Paused")
+            {
+                pauseCanvas.SetActive(false);
+                text.gameObject.SetActive(true);
+                gameState = "Starting";
+            }
         }
     }
 
@@ -43,6 +64,27 @@ public class LevelManager : MonoBehaviour
         {
             Instantiate(enemyTypes[0].gameObject, enemySpawnPoints[Random.Range(0, enemySpawnPoints.Length)].position, enemyTypes[0].gameObject.transform.rotation);
             nextSpawn = Time.time + spawnRate;
+        }
+    }
+
+    void CountDown()
+    {
+        timeLeft -= Time.deltaTime;
+        if (Mathf.Round(timeLeft) <= 3)
+        {
+            text.text = timeLeft.ToString("0");
+        }
+        else
+        {
+            text.text = "";
+        }
+
+        if (Mathf.Round(timeLeft) < 1)
+        {
+            text.text = "";
+            text.gameObject.SetActive(false);
+            timeLeft = 4f;
+            gameState = "Playing";
         }
     }
 }
